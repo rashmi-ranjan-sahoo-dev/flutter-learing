314bb074c8f3592e4b18d51fe1dbbbbb86462bed
rashmi-ranjan-sahoo-dev <rinkuasahoo04@gmail.com>
2026-07-23 20:23:46 +0530

    complete learning of besic flutter

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -12,68 +12,91 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String btnName = "Click me";
-  int curIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text("My Firts flutter app")),
-        body: curIndex == 0 ? Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.blue,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.deepOrange,
-                ),
-                onPressed: () {
-                  setState(() {
-                    btnName = btnName == "Click me" ? "Clicked" : "Click me";
-                  });
+      home: const MyAppExtention(),
+    );
+  }
+}
 
-                  debugPrint("Button Clicked");
-                },
-                child: Text(btnName),
+class MyAppExtention extends StatefulWidget {
+  const MyAppExtention({super.key});
+
+  @override
+  State<MyAppExtention> createState() => _MyAppExtentionState();
+}
+
+class _MyAppExtentionState extends State<MyAppExtention> {
+  String btnName = "Click me";
+  int curIndex = 0;
+  bool isChange = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("My Firts flutter app")),
+      body: curIndex == 0
+          ? Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.blue,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepOrange,
+                    ),
+                    onPressed: () {
+                      debugPrint("Button Clicked");
+                    },
+                    child: Text(btnName),
+                  ),
+
+                  const SizedBox(width: 20),
+
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => NextPage()),
+                      );
+                    },
+                    child: Text("Next Page"),
+                  ),
+                ],
               ),
-
-              const SizedBox(width: 20),
-
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    btnName = btnName == "Click me" ? "Clicked" : "Click me";
-                  });
-
-                  debugPrint("Button Clicked");
-                },
-                child: Text(btnName),
-              ),
-            ],
-          ),
-        ) : Image.asset('images/imageflutter.jpg'),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
-            BottomNavigationBarItem(
-              label: "Setting",
-              icon: Icon(Icons.settings),
-            ),
-          ],
-          currentIndex: curIndex,
-          onTap: (int index) {
-            setState(() {
-              curIndex = index;
-            });
-          },
-        ),
+            )
+          : GestureDetector(
+            onTap : ((){
+              setState(() {
+                isChange = !isChange;
+              });
+            }),
+            child:isChange ? Image.asset('images/imageflutter.jpg'): Image.network("https://images.unsplash.com/photo-1493612276216-ee3925520721?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tfGVufDB8fDB8fHww")),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: "Setting", icon: Icon(Icons.settings)),
+        ],
+        currentIndex: curIndex,
+        onTap: (int index) {
+          setState(() {
+            curIndex = index;
+          });
+        },
       ),
     );
+  }
+}
+
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar());
   }
 }
---
+++ navigator/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home Screen")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondScreen()),
+            );
+          },
+          child: const Text("Go to Second Screen"),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Second Screen")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text("Go Back"),
+        ),
+      ),
+    );
+  }
+}
